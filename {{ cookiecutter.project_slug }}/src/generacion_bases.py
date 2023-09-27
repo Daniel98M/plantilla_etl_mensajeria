d269099191310d9f7707ce5b8b9e0c20aea9ea2c
JuanFe <jmartinezbernal02@gmail.com>
2023-09-27 11:42:53 -0500

    Configurando el logging

--- {{ cookiecutter.project_slug }}/src/generacion_bases.py
+++ {{ cookiecutter.project_slug }}/src/generacion_bases.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 
-def run(dia):
+from Utils.functions import send_message, config_logging
+
+def run(dia, config):
+    logger = config_logging(dia)
+    logger.info('Ejecutando generación de bases')
+
     print(f'Ejecutando generación de bases el {dia}')
+    print(config)
 
 if __name__ == '__main__':
     run()
