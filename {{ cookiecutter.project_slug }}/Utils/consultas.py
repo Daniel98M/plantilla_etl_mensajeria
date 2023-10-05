788408e3fc51743da85d6bd0eb8996d0c6c67917
JuanFe <jmartinezbernal02@gmail.com>
2023-10-05 16:56:31 -0500

    Conexión a cloud

--- {{ cookiecutter.project_slug }}/Utils/consultas.py
+++ {{ cookiecutter.project_slug }}/Utils/consultas.py
@@ -14,35 +14,23 @@ with open(ruta_config, "r") as archivo:
 # Variables para la conexión
 ruta_instantclient = config['rutas']['ruta_instantclient']
 
-service_name = config['credenciales']['dwh_onpremise']['service_name']
-ip = config['credenciales']['dwh_onpremise']['ip']
-port = config['credenciales']['dwh_onpremise']['port']
+dsn_con = config['credenciales']['dwh_cloud']['dsn_con']
+usuario = config['credenciales']['dwh_cloud']['usuario']
+password = config['credenciales']['dwh_cloud']['password']
 
-usuario = config['credenciales']['dwh_onpremise']['usuario']
-password = config['credenciales']['dwh_onpremise']['password']
+# Conexión con la base de datos - CX_Oracle
+cx_Oracle.init_oracle_client(lib_dir="C:/Users/womsrvjm/Documents/instantclient_21_10")
 
+conn = cx_Oracle.connect(user=f'{usuario}', password=f'{password}', dsn=dsn_con)
+cur = conn.cursor()
+
+# Funciones
 def consulta(query):
-    # Conexión con la base de datos - CX_Oracle
-    cx_Oracle.init_oracle_client(config_dir = ruta_instantclient)
-    dsn_tns = cx_Oracle.makedsn(ip, port, service_name=service_name)
-    conn = cx_Oracle.connect(user=usuario, password=password, dsn=dsn_tns)
-    cur = conn.cursor()
-
     # Lectura del query
     df = pd.read_sql(query, con=conn)
     return(df)
 
 def registro_to_dwh(df, query):
-    # Conexión con la base de datos - CX_Oracle
-    cx_Oracle.init_oracle_client(config_dir = ruta_instantclient)
-    dsn_tns = cx_Oracle.makedsn(ip, port, service_name=service_name)
-    conn = cx_Oracle.connect(user=usuario, password=password, dsn=dsn_tns)
-    cur = conn.cursor()
-
-    # Conexión con sqlalchemy
-    oracle_db = sa.create_engine(f'oracle+cx_oracle://{usuario}:{password}@{ip}:{port}/?service_name={service_name}')
-    connection = oracle_db.connect()
-
     # Convertir DataFrame a lista de tuplas con valores convertidos a str y truncados
     rows = [tuple(str(val)[:2000] for val in row) for row in df.values]
 
