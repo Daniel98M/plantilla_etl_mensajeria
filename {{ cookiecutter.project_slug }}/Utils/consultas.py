bd8518412d046c71b30f392dc639220c84cf4546
JuanFe <jmartinezbernal02@gmail.com>
2025-03-14 23:02:17 -0500

    refactoring ⚒️: Ajuste de formatos y buenas prácticas

--- {{ cookiecutter.project_slug }}/Utils/consultas.py
+++ {{ cookiecutter.project_slug }}/Utils/consultas.py
@@ -1,28 +1,33 @@
-import pandas as pd
-import numpy as np
-
-import cx_Oracle
-import sqlalchemy as sa
+"""Modulo para conectarse a DWH Cloud"""
 
 import os
 import yaml
 
+import pandas as pd
+
+import cx_Oracle
+import sqlalchemy as sa
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Variables globales
 try:
-    RUTA_CONFIG = './config/config.yml'
-    with open(RUTA_CONFIG, "r", encoding='utf-8') as archivo:
+    ruta_config = './config/config.yml'
+    with open(ruta_config, "r", encoding='utf-8') as archivo:
         config = yaml.safe_load(archivo)
 except:
-    RUTA_CONFIG = '../config/config.yml'
-    with open(RUTA_CONFIG, "r", encoding='utf-8') as archivo:
+    ruta_config = '../config/config.yml'
+    with open(ruta_config, "r", encoding='utf-8') as archivo:
         config = yaml.safe_load(archivo)
 
 # Variables para la conexión
 ruta_instantclient = config['rutas']['ruta_instantclient']
 
-dsn_con = config['credenciales']['dwh_cloud']['dsn_con']
-usuario = config['credenciales']['dwh_cloud']['usuario']
-password = os.getenv('SQL')
+dsn_con = os.getenv('dwh_dsn')
+usuario = os.getenv('dwh_user')
+password = os.getenv('dwh_password')
 
 # Conexión con la base de datos - CX_Oracle
 cx_Oracle.init_oracle_client(lib_dir=ruta_instantclient)
@@ -31,12 +36,26 @@ conn = cx_Oracle.connect(user=f'{usuario}', password=f'{password}', dsn=dsn_con)
 cur = conn.cursor()
 
 # Funciones
-def consulta(query):
+def consulta(query: str) -> pd.DataFrame:
+    """Ejecuta un query SQL que retorna un dataframe
+
+    Args:
+        query (str): Query a ejecutar
+
+    Returns:
+        pd.DataFrame: Dataframe resultante de la consulta
+    """
     # Lectura del query
     df = pd.read_sql(query, con=conn)
     return(df)
 
-def registro_to_dwh(df, query):
+def registro_to_dwh(df: pd.DataFrame, query: str):
+    """Carga los registros de un DataFrame en el DWH
+
+    Args:
+        df (pd.DataFrame): DataFrame a cargar
+        query (str): Query de inserción
+    """
     # Convertir DataFrame a lista de tuplas con valores convertidos a str y truncados
     rows = [tuple(str(val)[:2000] for val in row) for row in df.values]
 
@@ -49,18 +68,34 @@ def registro_to_dwh(df, query):
         cur.executemany(query, chunk)
         conn.commit()
 
-def ejecutar_procedimiento(query):
-    cur.callproc(query)
+def ejecutar_procedimiento(procedimiento: str):
+    """Ejecuta un procedimiento SQL con parámetros
+
+    Args:
+        procedimiento (str): Nombre del procedimiento
+    """
+    cur.callproc(procedimiento)
     conn.commit()
 
-def ejecutar_procedimiento_parametros(query, parametros):
-    cur.callproc(query, parametros)
+def ejecutar_procedimiento_parametros(procedimiento: str, parametros: list):
+    """Ejecuta un procedimiento SQL con parámetros
+
+    Args:
+        procedimiento (str): Nombre del procedimiento
+        parametros (list): Lista de parámetros
+    """
+    cur.callproc(procedimiento, parametros)
     conn.commit()
 
-def ejecutar_query(query):
+def ejecutar_query(query: str):
+    """Ejecuta un bloque de código SQL
+
+    Args:
+        query (str): Query SQL
+    """
     cur.execute(query)
     conn.commit()
 
 def cerrar_conexion():
-    # Cerramos la conexión
+    """Cerrar la conexión"""
     conn.close()
--- {{ cookiecutter.project_slug }}/src/generacion_bases.py
+++ {{ cookiecutter.project_slug }}/src/generacion_bases.py
@@ -1,19 +1,17 @@
+from datetime import datetime
+
 import pandas as pd
 import numpy as np
 
 from Utils.functions import send_message, config_logging
 
-def run(config, fecha):
+def run(config: dict, fecha: datetime):
 
     # Variables globales
     dia = int(str(fecha).replace('-', '')[:8])
-    mes = int(str(fecha).replace('-', '')[:6])
 
     logger = config_logging(dia)
     logger.info('Ejecutando generación de bases')
 
     print(f'Ejecutando generación de bases el {dia}')
     print(config)
-
-if __name__ == '__main__':
-    run()
